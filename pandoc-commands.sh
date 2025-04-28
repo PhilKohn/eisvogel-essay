51e6d59b18292972cf87891fc9dfcafc488abdc5
PhilKohn <philipp_kohn@outlook.com>
2025-04-28 16:00:09 +0200

    Regular commit: 04/28/2025

--- pandoc-commands.sh
+++ pandoc-commands.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Main command (uncommented)
-pandoc --verbose "./src/input.md" -o "output.pdf" --template /.pandoc/templates/eisvogel.latex --metadata-file "./src/frontmatter.yml"
+#pandoc --verbose "./src/input.md" -o "output.pdf" --template /.pandoc/templates/eisvogel.latex --metadata-file "./src/frontmatter.yml"
 
-#pandoc --verbose "./src/input.md" -o "output.pdf" --template /.pandoc/templates/eisvogel.latex --metadata-file "./src/frontmatter.yml" --bibliography "./src/assets/assignment1.bib"
+pandoc --verbose "./src/input.md" -o "output.pdf" --template /.pandoc/templates/eisvogel.latex --metadata-file "./src/frontmatter.yml" --bibliography "./src/assets/assignment1.bib" --csl "./src/assets/apa.csl"
 
 # Additional commands (commented out)
 #pandoc "./src/input.md" -o "output.docx"
